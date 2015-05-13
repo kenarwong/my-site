719caae706372d7f1fd174eb67c4f758409f392c
Ken Hwang <kenhwang@Kens-MacBook.local>
2015-05-12 22:41:09 -0400

    added nav items

---
+++ models/navitem.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+//var db = mongoose.connection;
+//
+//db.on('error', console.error);
+//db.once('open', function() {
+//	console.log('db connected');
+//});
+//
+//// Connect to our mongo database
+//mongoose.connect('mongodb://localhost/mydb');
+
+// Create a new schema for our tweet data
+var schema = new mongoose.Schema({
+    navurl        : String
+  , friendlyname  : String
+}, { collection: 'nav' });
+
+var NavItem = mongoose.model('NavItem', schema);
+
+// make this available to our users in our Node applications
+module.exports = NavItem;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var NavItem = require('../models/navitem.js');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Demo Project' });
+router.get('/:nav?', function(req, res, next) {
+  NavItem.find({}, function(err, navitems){
+    //console.log(navitems);
+    var navurl = req.params.nav || 'home';
+    var navfilter = navitems.filter(function(obj) {return obj.navurl == navurl});
+    if (navfilter.length == 0) {
+      res.status(404).render('error', {
+        message: 'Not found',
+        error: {}
+      });
+      return console.log('not found: ' + req.params.nav);
+    } else {
+      var friendlyname = navfilter[0].friendlyname;
+      res.render('index', { 
+        title: friendlyname,
+        navitems: navitems
+      });
+    }
+  });
 });
 
 module.exports = router;
