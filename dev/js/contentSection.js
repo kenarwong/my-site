7e027ef35467b3b00ad6d1f7a9629e577763c03c
Ken Hwang <kenhwang@Kens-MacBook.local>
2015-10-15 08:05:08 -0400

    nav items query as promise

--- dev/js/contentSection.js
+++ dev/js/contentSection.js
@@ -1,96 +1,97 @@
-$(document).ready(function() {
-  React.render(
-      React.createElement(ContentBox, {url: "api/content"}), //pollInterval={2000}
-      document.getElementById('main-content')
-      );
-});
-
-var ContentBox = React.createClass({displayName: 'ContentBox',
-  loadContentFromServer: function() {
-  	$.ajax({
-  		url: this.props.url,
-  		dataType: 'json',
-  		success: function(data) {
-  			this.setState({data: data});
-  		}.bind(this),
-  		error: function(xhr, status, err) {
-  			console.error(this.props.url, status, err.toString());
-  		}.bind(this)
-  	});
-  },
-  getInitialState: function() {
-  	return {data: []};
-  }, 
-  componentDidMount: function() {
-  	this.loadContentFromServer();
-  	// setInterval(this.loadCommentsFromServer,this.props.pollInterval);
-  },
-  render: function() {
-    return (
-        React.createElement(ContentWrapper, {data: this.state.data})
-
-        //<div className="content-wrapper">
-        //  <div id="center-content">
-        //  </div>
-        //  <div id="left-content">
-        //  </div>
-        //  <div id="right-content">
-        //  </div>
-        //</div>
-        );
-  }
-});
-
-//<h3>Comments</h3>
-//<CommentForm onCommentSubmit={this.handleCommentSubmit} />
-//<CommentList data={this.state.data} />
-
-var ContentWrapper = React.createClass({displayName: 'ContentWrapper',
-	render: function() {
-		var contentNodes = this.props.data.map(function(content) {
-			 return (
-				React.createElement(Content, {identifier: content.id}, 
-					content.text
-				)	
-			);
-		});
-		return (
-		React.createElement("div", {className: "content-wrapper"}, contentNodes)
-		);
-	}
-});
+//$(document).ready(function() {
+//  React.render(
+//      var elem = document.getElementById('main-content')
+//      <ContentBox url="api/content" />, //pollInterval={2000}
+//      elem
+//      );
+//});
 //
-//var CommentForm = React.createClass({displayName: 'CommentForm',
-// 	 handleSubmit: function(e) {
-//    		e.preventDefault();
-//    		var author = this.refs.author.getDOMNode().value.trim();
-//    		var text = this.refs.text.getDOMNode().value.trim();
-//    		if (!text || !author) {
-//      			return;
-//    		}
-//			this.props.onCommentSubmit({author: author, text: text}); 
-//        	this.refs.author.getDOMNode().value = '';
-//        	this.refs.text.getDOMNode().value = '';
-//    },
+//var ContentBox = React.createClass({displayName: 'ContentBox',
+//  loadContentFromServer: function() {
+//  	$.ajax({
+//  		url: this.props.url,
+//  		dataType: 'json',
+//  		success: function(data) {
+//  			this.setState({data: data});
+//  		}.bind(this),
+//  		error: function(xhr, status, err) {
+//  			console.error(this.props.url, status, err.toString());
+//  		}.bind(this)
+//  	});
+//  },
+//  getInitialState: function() {
+//  	return {data: []};
+//  }, 
+//  componentDidMount: function() {
+//  	this.loadContentFromServer();
+//  	// setInterval(this.loadCommentsFromServer,this.props.pollInterval);
+//  },
+//  render: function() {
+//    return (
+//        <ContentWrapper data={this.state.data} />
+//
+//        //<div className="content-wrapper">
+//        //  <div id="center-content">
+//        //  </div>
+//        //  <div id="left-content">
+//        //  </div>
+//        //  <div id="right-content">
+//        //  </div>
+//        //</div>
+//        );
+//  }
+//});
+//
+////<h3>Comments</h3>
+////<CommentForm onCommentSubmit={this.handleCommentSubmit} />
+////<CommentList data={this.state.data} />
+//
+//var ContentWrapper = React.createClass({displayName: 'ContentWrapper',
 //	render: function() {
+//		var contentNodes = this.props.data.map(function(content) {
+//			 return (
+//				<Content identifier={content.id}>
+//					{content.text}
+//				</Content>	
+//			);
+//		});
 //		return (
-//		<div className="commentForm">
-//      		<form className="commentForm" onSubmit={this.handleSubmit}>
-//        		<input type="text" placeholder="Your name" ref="author" />
-//        		<input type="text" placeholder="Say something..." ref="text" />
-//        		<input type="submit" value="Post" />
-//      		</form>
-//		</div>
+//		<div className="content-wrapper">{contentNodes}</div>
 //		);
 //	}
 //});
-
-var Content = React.createClass({displayName: 'Content',
-  render: function() { 
-    return (
-        React.createElement("div", {id: this.props.identifier}, 
-          React.createElement("p", null, this.props.children.toString())
-        )
-        );
-  }
-});
+////
+////var CommentForm = React.createClass({displayName: 'CommentForm',
+//// 	 handleSubmit: function(e) {
+////    		e.preventDefault();
+////    		var author = this.refs.author.getDOMNode().value.trim();
+////    		var text = this.refs.text.getDOMNode().value.trim();
+////    		if (!text || !author) {
+////      			return;
+////    		}
+////			this.props.onCommentSubmit({author: author, text: text}); 
+////        	this.refs.author.getDOMNode().value = '';
+////        	this.refs.text.getDOMNode().value = '';
+////    },
+////	render: function() {
+////		return (
+////		<div className="commentForm">
+////      		<form className="commentForm" onSubmit={this.handleSubmit}>
+////        		<input type="text" placeholder="Your name" ref="author" />
+////        		<input type="text" placeholder="Say something..." ref="text" />
+////        		<input type="submit" value="Post" />
+////      		</form>
+////		</div>
+////		);
+////	}
+////});
+//
+//var Content = React.createClass({displayName: 'Content',
+//  render: function() { 
+//    return (
+//        <div id={this.props.identifier}>
+//          <p>{this.props.children.toString()}</p>
+//        </div>
+//        );
+//  }
+//});
--- routes/index.js
+++ routes/index.js
@@ -10,42 +10,52 @@ router.get('/:nav?', function(req, res, next) {
 
   //console.log(vm);
 
-  var navdata;
+  var navqry = NavItem.find({}).exec();
 
-  NavItem.find({}, function(err, navitems) {
+  navqry.addBack(function(){
+    console.log('nav query complete');
+  });
+
+  navqry.addErrback(function() {
+    console.log('uncaught error');
+    //return {httpcode: 500};
+  });
+
+  navqry.then(function(navitems) {
+    var navdata;
+
     //console.log(navitems);
     var navurl = req.params.nav || 'home';
     var navfilter = navitems.filter(function(obj) {return obj.navurl == navurl});
 
     if (navfilter.length == 0) {
-      res.status(404).render('error', {
-        message: 'Not found',
-        error: {}
-      });
-      return console.log('not found: ' + req.params.nav);
+      console.log('not found: ' + req.params.nav);
+      return {httpcode: 404};
     } else {
       navdata = {
         title: navfilter[0].friendlyname,
         thisurl: navfilter[0].navurl,
         navitems: navitems
-      }
+      };
 
       //console.log(navdata);
 
       if (Object.getOwnPropertyNames(navdata).length > 0) {
         //console.log(Object.getOwnPropertyNames(navdata));
-        Object.getOwnPropertyNames(navdata).forEach(function(e, i) {
-          //console.log(e);
-          vm[e] = navdata[e];
-        });
-      } else {
-        res.status(500).render('error', {
-          message: 'Error: Navigation data.',
-          error: {}
-        });
-      }
+        return {httpcode: 200, navdata: navdata};
 
+      } else {
+        console.log('navigation data error');
+        return {httpcode: 404};
+      }
+    }
+  }).then(function(data) {
+    if (data.httpcode == 200){
       //console.log(vm);
+      Object.getOwnPropertyNames(data.navdata).forEach(function(e, i) {
+        //console.log(e);
+        vm[e] = data.navdata[e];
+      });
 
       //console.log(req.app.get('env'));
       res.render('index', vm
@@ -55,8 +65,20 @@ router.get('/:nav?', function(req, res, next) {
           //  navitems: navitems,
           //}
           );
+    } else if (data.httpcode == 404) {
+      res.status(404).render('error', {
+        message: 'Not found',
+        error: {}
+      });
+    } else {
+      res.status(500).render('error', {
+        message: 'Oops, an error occured.',
+        error: {}
+      });
     }
   });
+
+
 });
 
 module.exports = router;
