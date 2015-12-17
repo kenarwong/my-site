6a5a0cf9aac7c6d999aa67f8bf151f84f54f8a95
Ken Hwang <kenhwang@Kens-MacBook.local>
2015-12-16 20:04:58 -0500

    setting up content api

--- routes/api.js
+++ routes/api.js
@@ -6,41 +6,41 @@ var fs = require('fs');
 
 /* Default */
 router.get('/', function(req, res) {
-	res.send(null);
+  res.send(null);
 });
 
 /* GET JSON data. */
 router.get('/comment', function(req, res) {
-	// fs.readFile('_data.json', function(err, data) {
-	// 	res.setHeader('Content-Type', 'application/json');
-	// 	res.send(data);
-	// });
-	Comment.find({}, function(err,docs){
-		res.json(docs);
-	});
+  // fs.readFile('_data.json', function(err, data) {
+  // 	res.setHeader('Content-Type', 'application/json');
+  // 	res.send(data);
+  // });
+  Comment.find({}, function(err,docs){
+    res.json(docs);
+  });
 });
 
 /* POST JSON data. */
 router.post('/comment', function(req, res) {
-	// fs.readFile('_data.json', function(err, data) {
-	// 	var contents = JSON.parse(data);
-	// 	contents.push(req.body)
-	// 	fs.writeFile('_data.json', JSON.stringify(contents, null, 4), function (err) {
-	// 		res.setHeader('Content-Type', 'application/json');
-	// 		res.setHeader('Cache-Control', 'no-cache');
-	// 		res.send(JSON.stringify(contents));
-	// 	});
-	// });
+  // fs.readFile('_data.json', function(err, data) {
+  // 	var contents = JSON.parse(data);
+  // 	contents.push(req.body)
+  // 	fs.writeFile('_data.json', JSON.stringify(contents, null, 4), function (err) {
+  // 		res.setHeader('Content-Type', 'application/json');
+  // 		res.setHeader('Cache-Control', 'no-cache');
+  // 		res.send(JSON.stringify(contents));
+  // 	});
+  // });
 
-	var comment = new Comment({
-		author: req.body.author,
-		text: req.body.text
-	});
-	comment.save(function (err, data) {
-	  if (err) return console.error(err);
-	  console.log(data);
-	  console.log('saved comment, author: ' + comment.author + ', text: ' + comment.text);
-	})
+  var comment = new Comment({
+    author: req.body.author,
+    text: req.body.text
+  });
+  comment.save(function (err, data) {
+    if (err) return console.error(err);
+    console.log(data);
+    console.log('saved comment, author: ' + comment.author + ', text: ' + comment.text);
+  })
 });
 
 //router.get('/content/:navurl?', function(req, res) {
@@ -50,5 +50,57 @@ router.post('/comment', function(req, res) {
 //    res.json(results);
 //  });
 //});
+
+
+/* GET home page. */
+router.get('/content/:nav?', function(req, res, next) {
+  var navurl = req.params.nav || 'home';
+
+  // Querybuilder for content
+  var contentqry = Content.find({'navurl': navurl}).sort({"order":1}).exec();
+
+  // dummy finalize cb fnc
+  contentqry.addBack(function(){
+    console.log('content query complete');
+  });
+
+  // dummy error cb fnc
+  contentqry.addErrback(function() {
+    console.log('uncaught error in content query');
+  });
+
+  contentqry.then(function(contentresults) {
+    if (contentresults.length == 0) {
+      console.log('content not found: ' + navurl);
+
+      res.status(404).render('error', {
+        message: 'Not found',
+        error: {}
+      });
+    } else {
+      //var contentdata = {};
+      //contentresults.map(function(obj){
+      //  contentdata[obj.id] = obj.text;
+      //});
+
+      //center-content: contentresults.filter(function(obj){return obj.id == 'center-content'}),
+      //left-content: contentresults.filter(function(obj){return obj.id == 'left-content'}),
+      //right-content: contentresults.filter(function(obj){return obj.id == 'right-content'})
+
+      //console.log(contentdata);
+
+      //Object.getOwnPropertyNames(contentdata).forEach(function(e, i) {
+      //  //console.log(e);
+      //  vm[e] = contentdata[e];
+      //});
+
+      //var reactHtml = React.renderToString(ReactPartial({}));
+      //console.log(reactHtml);
+      //contentdata['react-partial'] = reactHtml;
+
+      res.json(contentresults);
+    };
+  });
+});
 
 module.exports = router;
